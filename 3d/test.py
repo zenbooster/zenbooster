530f79c9f58f32b07da79634a27aa293455a022d
zenbooster <zenbooster@gmail.com>
2022-11-02 02:58:42 +0300

    3d/test.py: пробую перейти на сборки.

---
+++ 3d/test.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+from zencad import *
+import zencad.assemble
+from metric import *
+
+case = from_brep('./res/1590bb_open.brep')
+lid = from_brep('./res/1590bb_lid.brep')
+asm_case = zencad.assemble.unit()
+asm_case.add(case)
+asm_case.add(lid).set_color(0.5, 0.5, 0.5, 0.5)
+################################
+bread_board = from_brep('./res/bread-board-4x6cm.brep').up(30).left(20).back(1.62/2)
+pin_f = from_brep("./res/pin-f-2.54.brep")
+pin_m = from_brep("./res/pin-m-2.54.brep").rotateX(deg(-90)).up(8.4+1.62/2)
+clemma2 = from_brep('./res/KF128-2.54-6P.brep')
+
+asm_p_mod = zencad.assemble.unit()
+asm_p_mod.add((bread_board ^ box(21, 1.62, 53, True).left(2.54/2)).right(2.54/2).rotateX(deg(90))).set_color(0.5, 0.65, 0.5)
+asm_p_mod.add(clemma2.up(1.62/2+0.1).rotateZ(deg(-90)).forw(2.54*7).left(2.54*2)).set_color(0, 0.5, 0)
+
+p_mod = nullshape()
+pm = pin_m.back(2.54/2)
+pm = pm.back(2.54 * 9)
+pm20 = pm.right(2.54 * 3)
+for i in range(20):
+    p_mod += pm20
+    pm20 = pm20.forw(2.54)
+
+asm_p_mod.add(p_mod).set_color(0, 0, 0)
+
+p_mod = nullshape()
+pm7 = pm.left(2.54 * 2)
+for i in range(7):
+    p_mod += pm7
+    pm7 = pm7.forw(2.54)
+
+asm_p_mod.add(p_mod).set_color(0, 0, 0)
+asm_p_mod.relocate(rotateX(deg(-90)) * rotateZ(deg(-90)) * down(8.4+1.7/2 + 2.5) * left(0.7) * back(-4.3))
+################################
+asm_stuff = zencad.assemble.unit()
+asm_stuff.add(from_brep('./res/ttgo-t-energy-t18-v2.brep').rotateX(deg(-90)).moveY(-25))
+#asm_stuff.relocate(forw(20.4+2.54*5) * left(9.5+2.54*3))
+################################
+
+asm_stuff.link(asm_p_mod)
+asm_case.link(asm_stuff)
+
+asm_case.relocate(rotateX(deg(90)) * up(10))
+disp(asm_case)
+
+bread_board = from_brep('./res/bread-board-4x6cm.brep').up(30).left(20).back(1.62/2)
+pin_f = from_brep("./res/pin-f-2.54.brep")
+pin_m = from_brep("./res/pin-m-2.54.brep").rotateX(deg(-90)).up(8.4+1.62/2)
+clemma2 = from_brep('./res/KF128-2.54-6P.brep')
+stuffing = from_brep('./res/ttgo-t-energy-t18-v2.brep')
+stuffing += (
+    from_brep('./res/max98357a-adafruit.brep')+
+    from_brep('./res/2-Pin-connector.brep').rotateZ(deg(180)).rotateY(deg(180)).forw(6.3)
+).forw(20.4+2.54*5).left(9.5+2.54*3)
+
+
+pf20 = pin_f.moveY(29/2-2.54/2).moveX(0.6 - 10*2.54).down(2.5)
+pf7 = pf20.forw(5*2.54)
+
+for i in range(20):
+    stuffing += pf20
+    pf20 = pf20.moveX(2.54)
+
+for i in range(7):
+    stuffing += pf7
+    pf7 = pf7.moveX(2.54)
+
+stuffing = stuffing.moveY(-25)
+
+p_mod = bread_board ^ box(21, 1.62, 53, True).left(2.54/2)
+p_mod = p_mod.right(2.54/2)
+p_mod = p_mod.rotateX(deg(90))
+pm = pin_m.back(2.54/2)
+pm = pm.back(2.54 * 9)
+pm20 = pm.right(2.54 * 3)
+pm7 = pm.left(2.54 * 2)
+
+for i in range(20):
+    p_mod += pm20
+    pm20 = pm20.forw(2.54)
+
+for i in range(7):
+    p_mod += pm7
+    pm7 = pm7.forw(2.54)
+
+clemma2 = clemma2.up(1.62/2+0.1)
+clemma2 = clemma2.rotateZ(deg(-90))
+p_mod += clemma2.forw(2.54*7).left(2.54*2)
+p_mod = p_mod.rotateZ(deg(-90))
+p_mod = p_mod.left(0.7)
+p_mod = p_mod.back(4.3)
+p_mod = p_mod.down(8.4+1.7/2 + 2.5)
+
+stuffing += p_mod
+
+btn = metric_screw(30, 1, 10.5, True).down(10.5)
+#btn += cylinder(r=30/2 - 1, h=15.5).down(15.5)
+btn += cylinder(r=27.5/2, h=17.5).down(17.5)
+hex = linear_extrude(ngon(r=39.0/2, n=6), (0, 0, 3.8), True).rotateZ(deg(90))
+btn += hex.down(10.5/2)
+h=4.5
+btn += cylinder(r=34.1/2, h=h).chamfer(0.3, refs=[point3(0, 0, h)])
+d = 0.05
+btn -= cylinder(r=17.0/2 + 3.0, h=d).up(4.5-d)
+btn += cylinder(r=17.0/2, h=d).up(4.5-d)
+d = 0.1
+btn -= cylinder(r=17.0/2 - 1.0, h=d).up(4.5-d)
+
+btn = btn.forw(22).up(14)
+#model = btn + lid
+model = btn + case + stuffing.left(5)
+#display(model)
+#display(lid, color = (0.5, 0.5, 0.5, 0.5))
+#display(p_mod.left(5).bbox().shape(), color = (0.5, 0.5, 1.0, 0.5))
+show()
