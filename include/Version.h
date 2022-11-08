e1849a4397f8bef49ed82f67782804216f2af599
Ilya Goncharov <Igoncharov@aladdin.ru>
2022-11-08 18:10:45 +0300

    В platformio.ini добавил env_default.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.6.16"
+        #define VERSION "0.6.17"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-11-08 15:45:49.002163"
+        #define BUILD_TIMESTAMP "2022-11-08 18:05:16.648989"
     #endif
     
