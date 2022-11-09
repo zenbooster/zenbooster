c67af424d59736dcb8abbe2a789a6339c6a54491
zenbooster <zenbooster@gmail.com>
2022-11-09 23:39:11 +0300

    TBluetoothStuff: настройка размера стека под arduino, espidf.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.29"
+        #define VERSION "1.0.30"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-11-09 16:57:38.534326"
+        #define BUILD_TIMESTAMP "2022-11-09 22:34:51.929780"
     #endif
     
