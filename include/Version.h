6bfdba2b9c25125ec117f0fce8643d749cad4fba
zenbooster <zenbooster@gmail.com>
2022-11-10 22:59:59 +0300

    sdkconfig: убрал логирование bluetooth

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.0.43"
+        #define VERSION "1.0.44"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-11-10 16:26:45.719174"
+        #define BUILD_TIMESTAMP "2022-11-10 22:28:28.745874"
     #endif
     
